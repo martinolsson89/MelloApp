cae022daef54b021375fceaeca71dbcd90efbdf6
Martin Olsson <martinolsson89@gmail.com>
2024-11-23 17:35:33 +0100

    reset db

--- MelloApp.Server/Migrations/20241123163254_init.Designer.cs
+++ MelloApp.Server/Migrations/20241123163254_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MelloApp.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241115213659_init")]
+    [Migration("20241123163254_init")]
     partial class init
     {
         /// <inheritdoc />
